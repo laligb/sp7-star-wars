fec48bef0429732c5171644cbe93e62cafcf94a4
Lali Bibilashvili <lalibibilashvili@gmail.com>
2025-02-05 12:00:40 +0100

    exercise 7 done

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-
+import ProtectedRoute from "./ProtectedRoutes";
 import WelcomePage from "../pages/WelcomePage";
 import StarshipDetailPage from "../pages/StarshipDetailPage";
 import StarshipsPage from "../pages/StarshipsPage";
@@ -10,10 +10,25 @@ const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<WelcomePage />} />
-      <Route path="/starships" element={<StarshipsPage />} />
-      <Route path="/starship/:id" element={<StarshipDetailPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
+
+      <Route
+        path="/starships"
+        element={
+          <ProtectedRoute>
+            <StarshipsPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/starship/:id"
+        element={
+          <ProtectedRoute>
+            <StarshipDetailPage />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 };
