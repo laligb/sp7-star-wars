f275eda93570eb18fecbba34fdf97eadff21ab4f
Lali Bibilashvili <lalibibilashvili@gmail.com>
2025-02-08 02:11:42 +0100

    test initialized

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+/// <reference types="vitest" />
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     hmr: false,
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/setupTests.js",
   },
 });
